b5e3ac3564a75fec694719fb58a4c18242cc3f77
Igoryani4 <s.2i@mail.ru>
2022-08-19 23:18:34 +0300

    выполнили все задания

--- Program.cs
+++ Program.cs
@@ -16,4 +16,55 @@ else
 }
 
 
-//
+////Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+
+Console.WriteLine("Введите первое чило:");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число :");
+int num2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число :");
+int num3 = int.Parse(Console.ReadLine());
+
+int max = num1;
+
+if (num2 > max)
+{
+    max = num2;
+}
+if (num3 > max)
+{
+    max = num3;
+}
+Console.WriteLine($"Наибольшее из введенных чисел является {max}");
+
+
+
+// Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Ведите любое число:");
+int even = int.Parse(Console.ReadLine());
+
+if (even % 2 == 0)
+{
+    Console.WriteLine("Число является четным");
+}
+else
+{
+    Console.WriteLine("Число не является четным");
+}
+
+// Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N. 
+
+
+Console.WriteLine("Ведите любое число:");
+int number = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= number; i++)
+{
+    if ( i % 2 == 0)
+    {
+        Console.Write(i + ",");
+    }
+}
