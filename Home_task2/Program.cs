c53954c887ba2b3d56fa81325792fd5fe487d67e
Igoryani4 <s.2i@mail.ru>
2022-08-22 23:35:26 +0300

    add home task2

--- Home_task2/Program.cs
+++ Home_task2/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//Напишите программу, которая принимает на вход трёхзначное число и 
+//на выходе показывает вторую цифру этого числа.
+
+string number = Console.ReadLine();
+
+
+Console.WriteLine(number[2]);
+
+
+//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+string number2 = Console.ReadLine();
+
+int length = number2.Length;
+
+if(length < 3 ){
+    Console.WriteLine("третьей цыфры нет");
+}
+else 
+{
+    Console.WriteLine(number2[2]);
+}
+
+//Напишите программу, которая принимает на вход цифру, 
+//обозначающую день недели, и проверяет, является ли этот день выходным.
+
+string number3 = Console.ReadLine();
+
+if (number3 == 6 || 7){
+    System.Console.WriteLine("Да");
+}
+
+else
+{
+    System.Console.WriteLine("Нет");
+}
