1c0b8d256c66934f12b3184f36c051f7e772a6ba
Igoryani4 <s.2i@mail.ru>
2022-09-01 22:52:40 +0300

    выполнил первую задачу ДЗ 5

--- HomeTask5/Program.cs
+++ HomeTask5/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int [] ArrayThreeDigit (int size){
+    
+    int[] array = new int [size];
+    for (int i = 0; i < array.Length; i ++){
+        array[i] = new Random().Next(100 , 999);
+    }
+    return array;
+}
+
+int FindEven (int[] array){
+    int count = 0;
+    for( int i= 0 ; i < array.Length ; i ++){
+        if (array[i] % 2 == 0){
+            count ++;
+        }
+
+    }
+    return count;
+} 
+
+
+Console.WriteLine("Введите значение длины массива");
+int length = int.Parse(Console.ReadLine());
+
+int[] array = ArrayThreeDigit(length);
+
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.WriteLine(FindEven(array));
