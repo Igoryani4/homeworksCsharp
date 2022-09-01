fbd935a3c2ccb0d32a4a645503677a8e0ffb6b9c
Igoryani4 <s.2i@mail.ru>
2022-09-01 23:03:27 +0300

    выполнил вторую задачу ДЗ 5

--- HomeTask5/Program.cs
+++ HomeTask5/Program.cs
@@ -1,7 +1,7 @@
 ﻿//Задайте массив заполненный случайными положительными трёхзначными числами. 
 //Напишите программу, которая покажет количество чётных чисел в массиве.
 
-int [] ArrayThreeDigit (int size){
+/* int [] ArrayThreeDigit (int size){
     
     int[] array = new int [size];
     for (int i = 0; i < array.Length; i ++){
@@ -28,4 +28,38 @@ int length = int.Parse(Console.ReadLine());
 int[] array = ArrayThreeDigit(length);
 
 Console.WriteLine($"[{string.Join(", ", array)}]");
-Console.WriteLine(FindEven(array));
+Console.WriteLine(FindEven(array)); */
+
+
+
+// Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+
+int [] ArrayRand (int size){
+    
+    int[] array = new int [size];
+    for (int i = 0; i < array.Length; i ++){
+        array[i] = new Random().Next(1 , 999);
+    }
+    return array;
+}
+
+int FindSumOdd (int[] array){
+    int Sum = 0;
+    for( int i= 0 ; i < array.Length ; i ++){
+        if (i % 2 == 1){
+            Sum += array[i];
+        }
+
+    }
+    return Sum;
+} 
+
+
+Console.WriteLine("Введите значение длины массива");
+int length = int.Parse(Console.ReadLine());
+
+int[] array = ArrayRand (length);
+
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.WriteLine(FindSumOdd(array));
